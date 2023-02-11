3f2bdd9249d1e8ef0e0c265e6f4a508511e43065
iqbalpa <iqbalpahlevi16@gmail.com>
2023-02-12 06:37:54 +0700

    finished header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,27 @@ import Logo from "../../assets/netflix-logo.png";
 
 const Header = () => {
 	return (
-		<div className="flex flex-row justify-between">
-			<img src={Logo} alt="Netflix Logo" />
-			<div className="flex flex-row">
-				<select>
-					<option value="en">English</option>
-					<option value="fr">French</option>
+		<div className="bg-gradient-to-b from-black/50 to-transparent p-4 px-44 flex flex-row items-center justify-between">
+			<img src={Logo} alt="Netflix Logo" className="w-40" />
+			<div className="flex flex-row items-center">
+				<select className="m-2 mr-3 rounded-md p-2 w-52 text-white bg-slate-700/75">
+					<option value="en" className="bg-white text-black">
+						English
+					</option>
+					<option value="fr" className="bg-white text-black">
+						French
+					</option>
+					<option value="es" className="bg-white text-black">
+						Spanish
+					</option>
+					<option value="de" className="bg-white text-black">
+						German
+					</option>
+					<option value="it" className="bg-white text-black">
+						Italian
+					</option>
 				</select>
-				<button>Sign In</button>
+				<button className="bg-red-500 ml-3 hover:bg-red-600 duration-150 rounded-md text-white px-5 py-2">Sign In</button>
 			</div>
 		</div>
 	);
