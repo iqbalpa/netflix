a9b3b021ebb4fabcebf08c2e36c5d0d2ea1b84b5
iqbalpa <iqbalpahlevi16@gmail.com>
2023-02-12 16:21:57 +0700

    finshed the sections

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -3,21 +3,22 @@ import React from "react";
 const Section = ({ text1, text2, image, imageRight }) => {
 	return (
 		<>
+			<div className="h-2 bg-gray-800"></div>
 			{imageRight && (
-				<div className="grid grid-cols-2 bg-black text-white py-10 px-40 items-center">
+				<div className="grid grid-cols-2 bg-black text-white py-20 px-40 items-center">
 					<div className="text-center w-full">
-						<h1 className="text-5xl text-left tracking-wider font-extrabold mb-6">Enjoy on your TV.</h1>
-						<p className="text-3xl text-left">Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</p>
+						<h1 className="text-5xl text-left tracking-wider font-extrabold mb-6">{text1}</h1>
+						<p className="text-3xl text-left">{text2}</p>
 					</div>
-					<img alt="tv" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png" />
+					<img alt="tv" src={image} />
 				</div>
 			)}
 			{!imageRight && (
-				<div className="grid grid-cols-2 bg-black text-white py-10 px-40 items-center">
-					<img alt="tv" src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg" />
+				<div className="grid grid-cols-2 bg-black text-white py-20 px-40 items-center">
+					<img alt="tv" src={image} />
 					<div className="text-center w-full">
-						<h1 className="text-5xl text-left tracking-wider font-extrabold mb-6">Download your shows to watch offline.</h1>
-						<p className="text-3xl text-left">Save your favorites easily and always have something to watch.</p>
+						<h1 className="text-5xl text-left tracking-wider font-extrabold mb-6">{text1}</h1>
+						<p className="text-3xl text-left">{text2}</p>
 					</div>
 				</div>
 			)}
--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -4,13 +4,42 @@ import Main from "../../components/Main/Main";
 import Section from "../../components/Section/Section";
 
 const LandingPage = () => {
+	const sectionData = [
+		{
+			text1: "Enjoy on your TV.",
+			text2: "Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.",
+			image: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png",
+			imageRight: true,
+		},
+		{
+			text1: "Download your shows to watch offline.",
+			text2: "Save your favorites easily and always have something to watch.",
+			image: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg",
+			imageRight: false,
+		},
+		{
+			text1: "Watch everywhere.",
+			text2: "Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.",
+			image: "https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png",
+			imageRight: true,
+		},
+		{
+			text1: "Create profiles for kids.",
+			text2: "Send kids on adventures with their favorite characters in a space made just for them—free with your membership.",
+			image: "https://occ-0-6701-64.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABfpnX3dbgjZ-Je8Ax3xn0kXehZm_5L6-xe6YSTq_ucht9TI5jwDMqusWZKNYT8DfGudD0_wWVVTFLiN2_kaQJumz2iivUWbIbAtF.png?r=11f",
+			imageRight: false,
+		},
+	];
+
 	return (
 		<>
 			<div className="bg-landing-page">
 				<Header />
 				<Main />
+				{sectionData.map((section, index) => (
+					<Section key={index} text1={section.text1} text2={section.text2} image={section.image} imageRight={section.imageRight} />
+				))}
 				<div className="h-2 bg-gray-800"></div>
-				<Section />
 			</div>
 		</>
 	);
