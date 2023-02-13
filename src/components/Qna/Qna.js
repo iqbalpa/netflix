3c84da5a78cb18728eab8eadde371b9495508c26
iqbalpa <iqbalpahlevi16@gmail.com>
2023-02-13 07:59:39 +0700

    create QNA template

---
+++ src/components/Qna/Qna.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Qna = () => {
+	return (
+		<>
+			<div>
+				<div className="bg-gray-700 ">
+					<p>What is Netflix?</p>
+				</div>
+
+				<div className="bg-gray-700">
+					<p>
+						Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you
+						want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!
+					</p>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Qna;
--- src/layouts/Faq/Faq.js
+++ src/layouts/Faq/Faq.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Qna from "../../components/Qna/Qna";
 
 const Faq = () => {
-    return (
-        <></>
-    )
-}
+	return (
+		<>
+			<div className="bg-black h-96 py-20 px-52 text-white ">
+				<h1 className="text-5xl text-center font-extrabold mb-6">Frequently Asked Questions</h1>
+				<Qna />
+			</div>
+		</>
+	);
+};
 
 export default Faq;
