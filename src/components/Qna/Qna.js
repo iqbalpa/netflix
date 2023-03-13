7b838ad735846a646b16f74ea52df74aa651f256
iqbalpa <iqbalpahlevi16@gmail.com>
2023-03-13 07:29:58 +0700

    finished Qna card roughly

--- src/components/Qna/Qna.js
+++ src/components/Qna/Qna.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Qna = () => {
+	const [show, setShow] = useState(false);
+	const handleClick = () => {
+		setShow(!show);
+	};
+
 	return (
 		<>
 			<div>
-				<div className="bg-gray-700 ">
-					<p>What is Netflix?</p>
+				<div className="bg-gray-700 p-5 mb-1 flex flex-row justify-center items-center">
+					<p className="text-3xl ">What is Netflix?</p>
+					<button onClick={handleClick} className="text-5xl ml-auto">
+						{show ? "x" : "+"}
+					</button>
 				</div>
 
-				<div className="bg-gray-700">
-					<p>
-						Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever you
-						want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!
-					</p>
-				</div>
+				{show && (
+					<div className="bg-gray-700 p-5">
+						<p className="text-2xl">
+							Netflix is a streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices. You can watch as much as you want, whenever
+							you want without a single commercial – all for one low monthly price. There's always something new to discover and new TV shows and movies are added every week!
+						</p>
+					</div>
+				)}
 			</div>
 		</>
 	);
--- src/layouts/Faq/Faq.js
+++ src/layouts/Faq/Faq.js
@@ -4,7 +4,7 @@ import Qna from "../../components/Qna/Qna";
 const Faq = () => {
 	return (
 		<>
-			<div className="bg-black h-96 py-20 px-52 text-white ">
+			<div className="bg-black h-full py-20 px-52 text-white ">
 				<h1 className="text-5xl text-center font-extrabold mb-6">Frequently Asked Questions</h1>
 				<Qna />
 			</div>
