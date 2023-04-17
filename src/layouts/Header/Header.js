a71dc5cf128ce40c72912fbeb4759e5ad6687f1a
iqbalpa <iqbalpahlevi16@gmail.com>
2023-04-17 07:33:30 +0700

    refactor: language dropdown

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,28 +1,13 @@
 import React from "react";
 import Logo from "../../assets/netflix-logo.png";
+import LanguageDropdown from "../../components/LanguageDropdown/LanguageDropdown";
 
 const Header = () => {
 	return (
 		<div className="bg-gradient-to-b from-black/90 to-black/30 p-4 px-44 flex flex-row items-center justify-between">
 			<img src={Logo} alt="Netflix Logo" className="w-40" />
 			<div className="flex flex-row items-center">
-				<select className="m-2 mr-3 rounded-md p-2 w-52 text-white bg-slate-700/75">
-					<option value="en" className="bg-white text-black">
-						English
-					</option>
-					<option value="fr" className="bg-white text-black">
-						French
-					</option>
-					<option value="es" className="bg-white text-black">
-						Spanish
-					</option>
-					<option value="de" className="bg-white text-black">
-						German
-					</option>
-					<option value="it" className="bg-white text-black">
-						Italian
-					</option>
-				</select>
+				<LanguageDropdown />
 				<button className="bg-red-500 ml-3 hover:bg-red-600 duration-150 rounded-md text-white px-5 py-2">Sign In</button>
 			</div>
 		</div>
